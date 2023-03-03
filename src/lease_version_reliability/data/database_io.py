0aea249107be8c9b390ba0733cf623fe7bd662ac
Sagar Patel <sagarpatel@sagars-mbp.myfiosgateway.com>
2023-03-03 15:19:01 -0500

    updates

--- src/lease_version_reliability/data/database_io.py
+++ src/lease_version_reliability/data/database_io.py
@@ -76,7 +76,7 @@ async def batch_reliable_data(
         max=max,
     )
     data = [dict(item) for item in await db.fetch_all(query)]
-    data = pd.DataFrame(data)
+    # data = pd.DataFrame(data)
     # for col in data.columns:
     #     if col in settings.DATA_TYPE_DICT:
     #         if settings.DATA_TYPE_DICT[col] == "datetime64[ns]":
@@ -84,7 +84,7 @@ async def batch_reliable_data(
     #         else:
     #             data[col] = data[col].astype(settings.DATA_TYPE_DICT[col])
 
-    return data
+    return pd.DataFrame(data)
 
 
 async def batch_all_data(
@@ -101,14 +101,14 @@ async def batch_all_data(
     )
 
     data = [dict(item) for item in await db.fetch_all(query)]
-    data = pd.DataFrame(data)
+    # data = pd.DataFrame(data)
     # for col in data.columns:
     #     if col in settings.DATA_TYPE_DICT:
     #         if settings.DATA_TYPE_DICT[col] == "datetime64[ns]":
     #             data[col] = pd.to_datetime(data[col])
     #         else:
     #             data[col] = data[col].astype(settings.DATA_TYPE_DICT[col])
-    return data
+    return pd.DataFrame(data)
 
 
 async def get_reliable_data() -> pd.DataFrame:
